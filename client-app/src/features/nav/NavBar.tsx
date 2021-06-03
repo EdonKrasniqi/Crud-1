d4626ae79a115a80443f1c1342d9679018f62cc4
EdonKrasniqi <ek48587@ubt-uni.net>
2021-06-03 23:10:16 +0200

    navbar changes

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Button, Container, Menu, Header, Image, Dropdown } from "semantic-ui-react";
+import { Button, Container, Menu, Header, Image, Dropdown, Segment } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import LoginForm from "../users/LoginForm";
 import RegisterForm from "../users/RegisterForm";
@@ -9,21 +9,20 @@ import RegisterForm from "../users/RegisterForm";
 const NavBar: React.FC = () => {
   const {userStore: {user,logout,isLoggedIn},modalStore} = useStore(); 
   return (
-    <Menu fixed="top" inverted>
+    <Menu fixed='top' inverted pointing secondary>
       <Header as='h2' content='OneStopBuy' style={{marginTop: 10}} />
-      <Container>
-        <Menu.Item header as={NavLink} exact to="/">
-          Home
+      <Container style={{marginBottom:10}}>
+        <Menu.Item name='Home'  as={NavLink} exact to="/">
         </Menu.Item>
         <Menu.Item name="Accessories" as={NavLink} exact to="/accesories"></Menu.Item>
         <Menu.Item name="Auto" as={NavLink} exact to="/auto"></Menu.Item>
         <Menu.Item name="Library" as={NavLink} exact to="/library"></Menu.Item>
-        <Menu.Item name="Clothing" as={NavLink}exact to="/clothing"></Menu.Item>
+        <Menu.Item name="Clothing" as={NavLink} exact to="/clothing"></Menu.Item>
         <Menu.Item name="Health" as={NavLink} exact to="/health"></Menu.Item>
       </Container>
       {isLoggedIn ?(
         <>
-         <Menu.Item style={{marginRight:100}}>
+         <Menu.Item style={{marginRight:50}}>
            <Image src ={user?.image || '/assets/user.png'} avatar spaced ='right'/>
             <Dropdown pointing ='top left' text={user?.displayName}>
              <Dropdown.Menu>
